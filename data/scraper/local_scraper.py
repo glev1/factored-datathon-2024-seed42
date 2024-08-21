7f6e4b7883bf5159b3eb8fe5edb3371ef9e0e587
Ivan Alvarenga <ivan.alvarenga@dcc.ufrj.br>
2024-08-20 23:28:53 -0300

    feat: addition of parallel download

--- data/scraper/local_scraper.py
+++ data/scraper/local_scraper.py
@@ -1,11 +1,19 @@
 import argparse
 from datetime import datetime
 
-from utils import download_files, get_urls
+from utils import download_files, get_urls, add_col_to_event_data
+import os
+
+def list_files_recursive(directory):
+    file_list = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_list.append(os.path.join(root, file))
+    return file_list
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Short sample app')
-    parser.add_argument('--sd', action="store", dest='start_date', default="20240810")
+    parser.add_argument('--sd', action="store", dest='start_date', default="20240101")
     parser.add_argument('--ed', action="store", dest='end_date', default="20240813")
     args = parser.parse_args()
     start_date = datetime.strptime(args.start_date, "%Y%m%d")
--- data/scraper/utils.py
+++ data/scraper/utils.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
 urls = [
     "http://data.gdeltproject.org/gkg/index.html",
@@ -98,17 +99,52 @@ def unzip_file(file_path, folder_name):
         zip_ref.extractall(folder_name)
     os.remove(file_path)
 
-def download_files(urls: list[str]=[]):
-    bar = tqdm(desc="Downloanding files", total=len(urls), ncols=100)
-    for url in urls:
-        file_name = url.split("/")[-1]
-        date_str = file_name.split(".")[0]
-        folder_name = f"./files/{date_str}"
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
+def download_and_unzip(url):
+    file_name = url.split("/")[-1]
+    date_str = file_name.split(".")[0]
+    folder_name = f"./files/{date_str}"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
-        file_path = os.path.join(folder_name, file_name)
+    file_path = os.path.join(folder_name, file_name)
+    
+    # Check if the file already exists
+    if not os.path.exists(file_path):
         download_file(url, file_path, file_name)
         unzip_file(file_path, folder_name)
-        bar.update(1)
+    else:
+        print(f"File {file_name} already exists, skipping download.")
+    
+    return url
+
+def download_files(urls: list[str]=[]):
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(download_and_unzip, url) for url in urls]
+        for future in futures:
+            future.result()
     return
+
+
+def add_col_to_event_data(event_data_file: str, col_names: list[str] = [
+    "GLOBALEVENTID", "SQLDATE", "MonthYear", "Year", "FractionDate", "Actor1Code", 
+    "Actor1Name", "Actor1CountryCode", "Actor1KnownGroupCode", "Actor1EthnicCode", 
+    "Actor1Religion1Code", "Actor1Religion2Code", "Actor1Type1Code", "Actor1Type2Code", 
+    "Actor1Type3Code", "Actor2Code", "Actor2Name", "Actor2CountryCode", 
+    "Actor2KnownGroupCode", "Actor2EthnicCode", "Actor2Religion1Code", 
+    "Actor2Religion2Code", "Actor2Type1Code", "Actor2Type2Code", "Actor2Type3Code", 
+    "IsRootEvent", "EventCode", "EventBaseCode", "EventRootCode", "QuadClass", 
+    "GoldsteinScale", "NumMentions", "NumSources", "NumArticles", "AvgTone", 
+    "Actor1Geo_Type", "Actor1Geo_FullName", "Actor1Geo_CountryCode", 
+    "Actor1Geo_ADM1Code", "Actor1Geo_Lat", "Actor1Geo_Long", "Actor1Geo_FeatureID", 
+    "Actor2Geo_Type", "Actor2Geo_FullName", "Actor2Geo_CountryCode", 
+    "Actor2Geo_ADM1Code", "Actor2Geo_Lat", "Actor2Geo_Long", "Actor2Geo_FeatureID", 
+    "ActionGeo_Type", "ActionGeo_FullName", "ActionGeo_CountryCode", 
+    "ActionGeo_ADM1Code", "ActionGeo_Lat", "ActionGeo_Long", "ActionGeo_FeatureID", 
+    "DATEADDED", "SOURCEURL"
+] ):
+    with open(event_data_file, 'r') as file:
+        lines = file.readlines()
+        lines.insert(0, '\t'.join(col_names) + '\n')
+
+    with open(event_data_file, 'w') as file:
+        file.writelines(lines)
